40d5a66aab4c48afe47caa140ebe91bc9058214f
vidhangithub <vidhan.amc@gmail.com>
2021-08-14 22:18:40 +0100

    Pushed Skelleton Code

---
+++ vidmovieservice/src/main/java/com/vidhan/movie/vidmovieservice/VidmovieserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vidhan.movie.vidmovieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidmovieserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidmovieserviceApplication.class, args);
+	}
+
+}
---
+++ vidmovieservice/src/test/java/com/vidhan/movie/vidmovieservice/VidmovieserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhan.movie.vidmovieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidmovieserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
